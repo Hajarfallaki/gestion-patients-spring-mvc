75c1451d1b73db129cc7f480ceeea1b57cd0aacd
Hajarfallaki <hajarfallaki24@gmail.com>
2025-03-17 13:14:04 +0000

    Initial commit : Création du projet Spring MVC

---
+++ src/main/java/ma/enset/hospitale/HospitaleApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.hospitale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitaleApplication.class, args);
+    }
+
+}
