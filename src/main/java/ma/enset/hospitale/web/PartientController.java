8c23370d49c10ef2db5477ccfa40f9ad6af438cf
Hajarfallaki <hajarfallaki24@gmail.com>
2025-03-22 22:55:20 +0000

    Update PartientController and application.properties for H2 configuration

--- src/main/java/ma/enset/hospitale/web/PartientController.java
+++ src/main/java/ma/enset/hospitale/web/PartientController.java
@@ -1,4 +1,13 @@
 package ma.enset.hospitale.web;
 
+import ma.enset.hospitale.entities.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 public class PartientController {
+
+    @Controller
+    private PatientRespository patientRespository;
+
+
 }
