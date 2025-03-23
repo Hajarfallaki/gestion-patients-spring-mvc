1148f0581b332776dbde3b656509b5bdaa084f04
Hajarfallaki <hajarfallaki24@gmail.com>
2025-03-23 21:45:48 +0000

    Ajout et correction de l'authentification + mise à jour PatientController

--- src/main/java/ma/enset/hospitale/repository/PatientRepository.java
+++ src/main/java/ma/enset/hospitale/repository/PatientRepository.java
@@ -1,7 +1,18 @@
 package ma.enset.hospitale.repository;
 
 import ma.enset.hospitale.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    // Méthode générée par Spring Data JPA
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
+
+    // Méthode personnalisée avec une requête JPQL
+    @Query("select p from Patient p where p.nom like :x")
+    Page<Patient> chercher(@Param("x") String keyword, Pageable pageable);
 }
